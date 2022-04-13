f8b80d35bc6b633f4e77b2b0d2f41b27e0eda6e5
kishandew <kishan.dewangan123@gmail.com>
2022-04-13 17:42:52 +0530

    Initial Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  //0x369c3bD729F31C042A5f36414b690cf74e5f7082
